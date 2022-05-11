0d591a0c8d2497b24a182b57c93308455ae4d013
masontaylor7 <masonevantaylor@gmail.com>
2022-05-11 12:42:28 -0700

    ingredient model built

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from .db import db
 from .user import User
 from .category import Category
 from .recipe import Recipe
+from.ingredient import Ingredient
 from .collection import Collection
 from .collection_recipe import Collection_Recipe
