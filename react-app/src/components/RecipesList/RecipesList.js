da7554f8cb1d470cb390fc59c99082dccb720c5a
masontaylor7 <masonevantaylor@gmail.com>
2022-05-11 15:35:40 -0700

    recipes rendering to 'recipelist.js'

---
+++ react-app/src/components/RecipesList/RecipesList.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import './RecipesList.css'
+
+import { allRecipes } from '../../store/recipe';
+
+const RecipesList = () => {
+    const dispatch = useDispatch();
+    const recipes = Object.values(useSelector(state => state.recipes))
+
+    useEffect(() => {
+        dispatch(allRecipes())
+    }, [dispatch])
+
+    const imageStyle = {
+        width: "300px",
+        height: 'auto'
+    }
+
+    return (
+        <div>
+            {recipes.map(recipe => (
+                <div className='single_recipe'>
+                    <img src={recipe.image_url} style={imageStyle} />
+                    <div>
+                        <div className='recipe_title'>{recipe.title}</div>
+                        <div>{recipe.directions}</div>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+    );
+};
+
+export default RecipesList;
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session'
+import session from './session';
+import recipeReducer from './recipe';
 
 const rootReducer = combineReducers({
   session,
+  recipes: recipeReducer
 });
 
 
