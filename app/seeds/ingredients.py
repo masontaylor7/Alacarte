dd0d77fcb4282044764c0b37f5ee1b911e7e7c30
masontaylor7 <masonevantaylor@gmail.com>
2022-05-15 00:58:09 -0700

    added 4 recipes to db and seeded their coresponding ingredients

--- app/seeds/ingredients.py
+++ app/seeds/ingredients.py
@@ -58,8 +58,145 @@ def seed_ingredients():
     ing19 = Ingredient(
         recipe_id='2', amount='1/4', measurement='teaspoon', title='freshly ground black pepper'
     )
+    ing20 = Ingredient(
+        recipe_id='3', amount='1', measurement='large', title='sweet onion, roughly chopped (about 2 cups)'
+    )
+    ing21 = Ingredient(
+        recipe_id='3', amount='1/4', measurement='cup', title='vegetable oil'
+    )
+    ing22 = Ingredient(
+        recipe_id='3', amount='3', measurement='', title='anahiem peppers, divided'
+    )
+    ing23 = Ingredient(
+        recipe_id='3', amount='2', measurement='tablespoons', title='tomato paste'
+    )
+    ing24 = Ingredient(
+        recipe_id='3', amount='2', measurement='tablespoons', title='curry powder'
+    )
+    ing25 = Ingredient(
+        recipe_id='3', amount='1', measurement='teaspoon', title='advieh (persian spice)'
+    )
+    ing26 = Ingredient(
+        recipe_id='3', amount='1/2', measurement='teaspoon', title='felfel, or 1/4 teaspoon cayenne pepper'
+    )
+    ing27 = Ingredient(
+        recipe_id='3', amount='1/2', measurement='teaspoon', title='freshly ground black pepper'
+    )
+    ing28 = Ingredient(
+        recipe_id='3', amount='1/4', measurement='teaspoon', title='ground turmeric'
+    )
+    ing29 = Ingredient(
+        recipe_id='3', amount='1', measurement='large', title='cauliflower, cut into 2-inch florets'
+    )
+    ing30 = Ingredient(
+        recipe_id='3', amount='3/4', measurement='cup', title='water'
+    )
+    ing31 = Ingredient(
+        recipe_id='3', amount='1', measurement='teaspoon', title='kosher salt, plus more to taste'
+    )
+    ing32 = Ingredient(
+        recipe_id='3', amount='', measurement='', title='barbari or lavash, for serving'
+    )
+    ing33 = Ingredient(
+        recipe_id='4', amount='2', measurement='large', title='eggs'
+    )
+    ing34 = Ingredient(
+        recipe_id='4', amount='1', measurement='tablespoon', title='vegetable oil'
+    )
+    ing35 = Ingredient(
+        recipe_id='4', amount='1', measurement='small', title='yellow onion, cut into 1/2-inch halfmoons'
+    )
+    ing36 = Ingredient(
+        recipe_id='4', amount='3', measurement='', title='green cabbage leaves, cut into 2-inch squares'
+    )
+    ing37 = Ingredient(
+        recipe_id='4', amount='2', measurement='cloves', title='garlic, minced'
+    )
+    ing38 = Ingredient(
+        recipe_id='4', amount='1 1/2', measurement='cups', title='water'
+    )
+    ing39 = Ingredient(
+        recipe_id='4', amount='1', measurement='pound (about 3 cups)', title='garaetteok (cylindrical Korean rice cakes)'
+    )
+    ing40 = Ingredient(
+        recipe_id='4', amount='2', measurement='', title='scallions, ends trimmed and cut into 2-inch pieces'
+    )
+    ing41 = Ingredient(
+        recipe_id='4', amount='3', measurement='tablespoons', title='gochujang'
+    )
+    ing42 = Ingredient(
+        recipe_id='4', amount='1', measurement='tablespoon', title='soy sauce, plus more to taste'
+    )
+    ing43 = Ingredient(
+        recipe_id='4', amount='2', measurement='teaspoons', title='sugar, plus more to taste'
+    )
+    ing44 = Ingredient(
+        recipe_id='4', amount='1', measurement='teaspoon', title='toasted sesame oil'
+    )
+    ing45 = Ingredient(
+        recipe_id='4', amount='1', measurement='teaspoon', title='toasted sesame seeds'
+    )
+    ing46 = Ingredient(
+        recipe_id='5', amount='', measurement='', title='nonstick cooking spray, for greasing the pan'
+    )
+    ing47 = Ingredient(
+        recipe_id='5', amount='1', measurement='teaspoon', title='instant espresso powder'
+    )
+    ing48 = Ingredient(
+        recipe_id='5', amount='1 1/2', measurement='cups', title='semisweet chocolate chips, divided'
+    )
+    ing49 = Ingredient(
+        recipe_id='5', amount='1/2', measurement='cup', title='boiling water'
+    )
+    ing50 = Ingredient(
+        recipe_id='5', amount='1', measurement='cup', title='dark brown sugar, tightly packed'
+    )
+    ing51 = Ingredient(
+        recipe_id='5', amount='2', measurement='sticks', title='unsalted butter, melted'
+    )
+    ing52 = Ingredient(
+        recipe_id='5', amount='2', measurement='tablespoons', title='vegetable oil'
+    )
+    ing53 = Ingredient(
+        recipe_id='5', amount='1', measurement='tablespoon', title='vanilla extract'
+    )
+    ing54 = Ingredient(
+        recipe_id='5', amount='2', measurement='large', title='eggs, at room temperature'
+    )
+    ing55 = Ingredient(
+        recipe_id='5', amount='1 1/4', measurement='cups', title='all-purpose flour'
+    )
+    ing56 = Ingredient(
+        recipe_id='5', amount='1/3', measurement='cup', title='unsweetend natural cocoa powder'
+    )
+    ing57 = Ingredient(
+        recipe_id='5', amount='1/2', measurement='teaspoon', title='baking soda'
+    )
+    ing58 = Ingredient(
+        recipe_id='5', amount='1/2', measurement='teaspoon', title='kosher salt'
+    )
+    ing59 = Ingredient(
+        recipe_id='6', amount='1', measurement='cup', title='piloncillo or light or dark brown sugar'
+    )
+    ing60 = Ingredient(
+        recipe_id='6', amount='2', measurement='(2-inch)', title='cinnamon sticks'
+    )
+    ing61 = Ingredient(
+        recipe_id='6', amount='5', measurement='whole', title='cloves'
+    )
+    ing62 = Ingredient(
+        recipe_id='6', amount='2', measurement='quarts', title='filtred water, divided. No idea what that means..'
+    )
+    ing63 = Ingredient(
+        recipe_id='6', amount='1', measurement='large', title='organic pineapple'
+    )
+    ing64 = Ingredient(
+        recipe_id='6', amount='', measurement='', title='Pineapple slices, for garnish (optional)'
+    )
 
-    db.session.add_all([ing1, ing2, ing3, ing4, ing5, ing6, ing7, ing8, ing9, ing10, ing11, ing12, ing13, ing14, ing15, ing16, ing17, ing18, ing19])
+
+
+    db.session.add_all([ing1, ing2, ing3, ing4, ing5, ing6, ing7, ing8, ing9, ing10, ing11, ing12, ing13, ing14, ing15, ing16, ing17, ing18, ing19, ing20, ing21, ing22, ing23, ing24, ing25, ing26, ing27, ing28, ing29, ing30, ing31, ing32, ing33, ing34, ing35, ing36, ing37, ing38, ing39, ing40, ing41, ing42, ing43, ing44, ing45, ing46, ing47, ing48, ing49, ing50, ing51, ing52, ing53, ing54, ing55, ing56, ing57, ing58, ing59, ing60, ing61, ing62, ing63, ing64])
 
     db.session.commit()
 
